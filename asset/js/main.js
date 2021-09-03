d77bb4f50ee785342ecad1ddb47e721551936e7d
darlinf <58644216+darlinf@users.noreply.github.com>
2021-09-03 15:21:58 -0700

    Add files via upload

--- asset/js/main.js
+++ asset/js/main.js
@@ -57,3 +57,14 @@ boxes.forEach((item, i) => {
     item.classList.remove("box-z-indez");
   });
 });
+
+// Prepare the body tag by adding a "js-paused" class
+document.body.className += " js-loading";
+
+// Listen for when everything has loaded
+window.addEventListener("load", showPage, false);
+
+function showPage() {
+  // Remove the "js-paused" class
+  document.body.className = document.body.className.replace("js-loading", "");
+}
